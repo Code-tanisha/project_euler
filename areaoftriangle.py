4c55db862450e614f6d0f4776d3cf6ffef697167
Tanisha Gupta <37878240+Tanisha2910@users.noreply.github.com>
2018-12-09 12:05:16 +0530

    Program to print area of triangle

--- areaoftriangle.py
+++ areaoftriangle.py
@@ -2,5 +2,6 @@ a = float(input('Enter first side: '))
 b = float(input('Enter second side: '))
 c = float(input('Enter third side: '))
 s = (a+b+c)/2
-area = (s(s-a)*(s-b)*(s-c))**0.5
-print('The area of triangle is: ' area)
+# calculate the area
+area = (s*(s-a)*(s-b)*(s-c)) ** 0.5
+print('The area of the triangle is %0.2f' %area)
