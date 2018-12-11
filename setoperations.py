a2c6e9700ba9a7635e199540729822a8cab8f859
Tanisha Gupta <37878240+Tanisha2910@users.noreply.github.com>
2018-12-11 17:05:00 +0530

    Program to implement set operations.

---
+++ setoperations.py
@@ -0,0 +1,10 @@
+# Program to illustrate ALL different set operations
+
+E = {0,1,4,7,8};
+I = {3,6,9,3,5};
+# set union
+print("Union of E and I is: " E | I)
+# set intersection
+print("Intersection of E and I is: " E & I)
+# set difference
+print(Difference of E and I is : " E - I)
